c1f39854e3016175431b2cf95027dee265417d42
Renan Landero <61968856+jrrlandero@users.noreply.github.com>
2023-03-05 00:00:12 +0800

    Update script.js

--- script.js
+++ script.js
@@ -29,5 +29,11 @@ function isElementInView(element) {
 
 window.addEventListener('scroll', animateOnScroll);
 
+window.addEventListener("load", function() {
+  var preloader = document.getElementById("preloader");
+  preloader.style.display = "none";
+});
+
+
 
 

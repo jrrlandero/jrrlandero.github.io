faeada77a588f6ed594fb9f14132415ccb99a82c
Renan Landero <61968856+jrrlandero@users.noreply.github.com>
2023-03-05 02:27:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,23 @@ navLinks.forEach(link => {
 });
 
 
-window.addEventListener('load', function() {
-  const preloader = document.querySelector('preloader');
+function stopLoader() {
+  const preloader = document.getElementById('preloader');
+  const snake = document.getElementById('snake');
+  const preloaderBefore = document.querySelector('#preloader:before');
+
   preloader.classList.add('hide-preloader');
+  snake.classList.remove('animate-snake');
+
+  // hide the preloader:before pseudo-element after 2 seconds
+  setTimeout(() => {
+    preloaderBefore.style.display = 'none';
+  }, 2000);
+}
+
+
+window.addEventListener('load', function() {
+  stopLoader();
 });
 
 

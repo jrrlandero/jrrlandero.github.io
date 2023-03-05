4eec47fb50575181ae04a63822f786052eeef5da
Renan Landero <61968856+jrrlandero@users.noreply.github.com>
2023-03-05 23:16:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,17 +17,18 @@ function stopLoader() {
   const snake = document.querySelector('#snake');
   const preloaderBefore = document.querySelector('#preloader:before');
 
-  // Hide the preloader by adding the 'hide-preloader' class
-  preloader.classList.add('hide-preloader');
+  // Hide the preloader:before pseudo-element by adding the 'hide-preloader' class
+  preloaderBefore.classList.add('hide-preloader');
 
   // Remove the animation class from the snake
   snake.classList.remove('animate-snake');
 
-  // Hide the preloader:before pseudo-element after 2 seconds
+  // Hide the preloader container after 2 seconds
   setTimeout(() => {
-    preloaderBefore.style.display = 'none';
+    preloader.style.display = 'none';
   }, 2000);
 }
+
 
 
 // Add an event listener to window that triggers the stopLoader function when the page is loaded

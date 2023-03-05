c41c4c3e2e3231d317694754cfce971334bf5751
Renan Landero <61968856+jrrlandero@users.noreply.github.com>
2023-03-06 02:55:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,19 +13,24 @@ navLinks.forEach(link => {
 
 // Function to stop the preloader
 function stopLoader() {
+  console.log('stopLoader() called');
   const preloader = document.querySelector('#preloader');
   const snake = document.querySelector('#snake');
 
   preloader.classList.add('hide-preloader'); // add hide-preloader class to preloader element
+  console.log('hide-preloader class added');
 
   // Remove the animation class from the snake
   snake.style.animation = 'none';
+  console.log('animation removed from snake');
 
   // Hide the preloader after 2 seconds
   setTimeout(() => {
     preloader.style.display = 'none';
+    console.log('preloader hidden');
   }, 2000);
 }
+
 
 // Add an event listener to window that triggers the stopLoader function when the page is loaded
 window.addEventListener('load', () => {

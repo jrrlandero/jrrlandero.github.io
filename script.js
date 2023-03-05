28c031940e7d9d82dbd52e5d634daf0615dc9abf
Renan Landero <61968856+jrrlandero@users.noreply.github.com>
2023-03-05 12:43:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,11 @@ window.addEventListener('load', () => {
  stopLoader();
 }); 
 
+const hamburger = document.querySelector('.nav__hamburger');
+const menu = document.querySelector('.nav__menu');
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('nav__menu--active');
+});
+
+
